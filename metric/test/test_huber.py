ef6e1acdc372245bb960cdb280872fc29ca3725a
pradanaadn <pradanaadn21@gmail.com>
2025-01-09 21:07:24 +0800

    Update: add cross entropy

--- metric/test/test_huber.py
+++ metric/test/test_huber.py
@@ -1,13 +1,13 @@
 from metric.implementation.Huber import huber_loss
 
 
-def test_mse_list_float():
+def test_huber_list_float():
     y_true = [1.08, 1.2, 1.4, 2.1, 1.9, 7, 2.9]
     y_pred = [0.7, 1.1, 1.5, 1.9, 2.3, 2.7, 3.1]
     
-    mse = round(huber_loss(y_true, y_pred, 1.35), 3)
+    huber = round(huber_loss(y_true, y_pred, 1.35), 3)
     expected_result = 0.728
-    assert expected_result == mse, f"Expected {expected_result}, but got {mse}"
+    assert expected_result == huber, f"Expected {expected_result}, but got {huber}"
 
 def test_huber_loss_zero_difference():
     y_true = [1.0, 2.0, 3.0]
