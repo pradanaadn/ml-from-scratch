c90a4fa0b4c5d3798e004da5230f2b92561aad3c
pradanaadn <pradanaadn21@gmail.com>
2025-01-09 21:29:03 +0800

    Update: add accuracy

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
 test-uv:
 	uv run pytest .
-
+	make clean
 test:
 	pytest .
+	make clean
+
+clean:
+	find . -type d -name "__pycache__" -exec rm -r {} +
+	
--- metric/implementation/accuracy.py
+++ metric/implementation/accuracy.py
@@ -5,7 +5,10 @@ def accuracy(y_true, y_pred):
     
     len_y_true = len(y_true)
     len_y_false = len(y_pred)
+    
     if len_y_true!=len_y_false:
         raise ValueError('Make sure y_true and y_pred have same length')
+    if len_y_true == 0 and len_y_false == 0:
+        return 0
     acc = np.mean(np.equal(y_true, y_pred))
     return acc
